4967d877c46dd10cec669deea721f141c4c85a50
Anwesh43 <anweshthecool0@gmail.com>
2018-05-03 10:03:55 +0530

    adding SquareToCornerStage class and State class

---
+++ square-to-corner.ts
@@ -0,0 +1,55 @@
+const w : number = window.innerWidth, h : number = window.innerHeight, size : number = Math.min(w, h)/3
+class SquareToCornerStage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+
+    context : CanvasRenderingContext2D
+
+    constructor() {
+        this.initCanvas()
+    }
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = '#212121'
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+}
+
+class State {
+
+    scale : number = 0
+
+    dir : number = 0
+
+    prevScale : number = 0
+
+    update(stopcb : Function) {
+        this.scale += this.dir * 0.1
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            stopcb()
+        }
+    }
+
+    startUpdating(startcb : Function) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            startcb()
+        }
+    }
+}
